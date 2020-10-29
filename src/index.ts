ddfee7d1d9f23eaf8fd33d037b408244cf55cd6b
limichange <limichange@hotmail.com>
2020-10-29 16:59:21 +0800

    chore: toftcode => toft-code

--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,9 @@
-import { generate } from '@toftcode/swagger-axios-ts-generator'
+import { generate } from '@toft-code/swagger-axios-ts-generator'
 
 generate({
   out: './testServices',
   url:
-    'https://raw.githubusercontent.com/toftcode/swagger-axios-ts-generator/master/test.json',
+    'https://raw.githubusercontent.com/toft-code/swagger-axios-ts-generator/master/test.json',
   operationIdForeach: (operationId: string) => {
     return operationId.match(/[a-zA-Z]+/g)?.join('') ?? operationId
   },
